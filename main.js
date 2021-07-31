a88df4d6fbb766eb7be062369f2cfccf6c9b86f5
Link-HG <matiasngallardo@gmail.com>
2021-07-31 03:31:38 +0000

    1.1

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-const paymenMethod = document.querySelectorAll(".payment-method div")
-let currentlySelected;
-paymenMethod.forEach(element => {
-  if (element.classList.contains("check-selected")) {
-    currentlySelected = element;
-  }
-  element.addEventListener("click", function() {
-    currentlySelected.classList.remove("check-selected");
-    currentlySelected = this;
-    this.classList.add("check-selected")
-    this.querySelector("input[type='radio']").checked = true;
-  }, false);
+const inputFormTogglePassword = document.querySelectorAll(".form-question button");
+
+inputFormTogglePassword.forEach((element) => {
+  element.addEventListener("click", function () {
+      const img = this.querySelector("img");
+      const input = this.parentElement.querySelector("input");
+
+      if (input.type === "password") {
+        input.type = "text";
+        img.src = "img/hide-password.svg";
+      } else {
+        input.type = "password";
+        img.src = "img/show-password.svg";
+      }
+    },
+    false
+  );
 });
